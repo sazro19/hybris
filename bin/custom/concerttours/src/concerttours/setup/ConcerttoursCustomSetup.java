67725d5f5b6dfdcc8f5ec82c61909d848d2bc20d
Alex <sasha.zalesskiy@mail.ru>
2021-09-23 18:57:19 +0300

    reformatted

--- bin/custom/concerttours/src/concerttours/setup/ConcerttoursCustomSetup.java
+++ bin/custom/concerttours/src/concerttours/setup/ConcerttoursCustomSetup.java
@@ -11,52 +11,47 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 
 @SystemSetup(extension = "concerttours")
-public class ConcerttoursCustomSetup
-{
+public class ConcerttoursCustomSetup {
     private static final Logger LOG = LoggerFactory.getLogger(ConcerttoursCustomSetup.class);
     private ImportService importService;
-    public ImportService getImportService()
-    {
+
+    public ImportService getImportService() {
         return importService;
     }
-    public void setImportService(final ImportService importService)
-    {
+
+    public void setImportService(final ImportService importService) {
         this.importService = importService;
     }
+
     @SystemSetup(type = SystemSetup.Type.ESSENTIAL)
-    public boolean putInMyEssentialData()
-    {
+    public boolean putInMyEssentialData() {
         LOG.info("Starting custom essential data loading for the Concerttours extension");
         LOG.info("Custom essential data loading for the Concerttours extension completed.");
         return true;
     }
+
     @SystemSetup(type = SystemSetup.Type.PROJECT)
-    public boolean addMyProjectData()
-    {
+    public boolean addMyProjectData() {
         LOG.info("Starting custom project data loading for the Concerttours extension");
         impexImport("/impex/concerttours-bands.impex");
         impexImport("/impex/concerttours-yBandTour.impex");
         LOG.info("Custom project data loading for the Concerttours extension completed.");
         return true;
     }
-    protected boolean impexImport(final String filename)
-    {
+
+    protected boolean impexImport(final String filename) {
         final String message = "Concerttours impexing [" + filename + "]...";
-        try (final InputStream resourceAsStream = getClass().getResourceAsStream(filename))
-        {
+        try (final InputStream resourceAsStream = getClass().getResourceAsStream(filename)) {
             LOG.info(message);
             final ImportConfig importConfig = new ImportConfig();
             importConfig.setScript(new StreamBasedImpExResource(resourceAsStream, "UTF-8"));
             importConfig.setLegacyMode(Boolean.FALSE);
             final ImportResult importResult = getImportService().importData(importConfig);
-            if (importResult.isError())
-            {
+            if (importResult.isError()) {
                 LOG.error(message + " FAILED");
                 return false;
             }
-        }
-        catch (final Exception e)
-        {
+        } catch (final Exception e) {
             LOG.error(message + " FAILED", e);
             return false;
         }
